04d15ac7d6dbf126883931d72206f32939d1e1dc
Dana <dmbrante@gmail.com>
2026-06-12 15:27:27 -0400

    ok3

---
+++ ejercicio1.py
@@ -0,0 +1,22 @@
+#- ingresar
+#En una lista ingresar diferentes mascotas, cada mascota tendrá: chip / nombre / raza
+#Validación:
+#-chip: numero entero mayor a cero
+#- sin espacio y minimo 3 letras
+#sin espacios y minimo 3 letras
+#si todo esta ok, se debe almacenar en un listado
+
+#todo con metodos (funciones def) separado
+
+#Menu()
+#- seleccione()
+#-Rutas()
+#   |
+#   1-IngresarMascota(lista[])
+#       |
+#       |-Pedir el valor del chip
+#           |-validarChip()
+#       |Pedir nombre
+#           |-ValidarNombre()
+#       |Pedir Raza
+#           |ValidarRaza()
